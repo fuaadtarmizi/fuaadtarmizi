78a93b186a6c234e4eacd55f04c745e3b0c09d60
fuaadtarmizi <fuaad2120@gmail.com>
2024-08-30 16:43:02 +0800

    test8

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -102,7 +102,37 @@ const Home = () => {
               <li className="mb-12">
                 <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                   <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                  <header className="z-10 mb-2 mt-1 text-sm font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2018 to Present">2022 — Present</header>
+                  <header className="z-10 mb-2 mt-1 text-sm font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2018 to Present">2023 — current</header>
+                  <div className="z-10 sm:col-span-6">
+                    <h3 className="font-medium leading-snug text-slate-200">
+                      <div>
+                      <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base" href="https://www.yegmy.com/" target="_blank" rel="noreferrer" aria-label="Lead Engineer at Upstatement">
+                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                        <span className="text-base">Senior Executive of Web Developer<w className="p-1"></w> | <w className="p-1"></w>YEG Academy Sdn Bhd 
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
+                          <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path>
+                          </svg>
+                          </span>
+                          </a>
+                      </div>
+                    </h3>
+                    <p className="mt-2 text-base leading-normal">Involve in designing software systems, including defining
+                      architecture, setting up database or creating scalable
+                      application. Other,Solved critical bugs and improve application performance,
+                      especially in coding or in web development, ensuring optimal
+                      use experiences. In the term full-stack, I enhanced my full-stack project, including backend using
+                      Node.js, Laravel and implemented modern frontend framework. Also that my responsiblelity to developed responsive web systems development and
+                      application phone, integrate API and continuously learned and
+                      adapted new technologies</p>
+                    <div>
+                      </div>
+                      </div>
+                      </div>
+                      </li>
+              <li className="mb-12">
+                <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                  <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                  <header className="z-10 mb-2 mt-1 text-sm font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2018 to Present">2022 — 2023</header>
                   <div className="z-10 sm:col-span-6">
                     <h3 className="font-medium leading-snug text-slate-200">
                       <div>
@@ -123,12 +153,14 @@ const Home = () => {
                         Developing and maintaining a reporting system for support
                         users, managers, and executives to keep track of support calls
                         both on a moment-to-moment basis and over time using PHP,
-                        MySQL, HTML, and CSS..</p>
+                        MySQL, HTML, and CSS.</p>
                     <div>
                       </div>
                       </div>
                       </div>
                       </li>
+
+
                       <li className="mb-12">
                 <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                   <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
